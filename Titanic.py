ad4b29426a1ffd96a75e8d25b6d31ecf8940f0c6
kal-yi <johnkalargaris@gmail.com>
2020-10-25 22:26:13 +0000

    add plot

--- Titanic.py
+++ Titanic.py
@@ -1,3 +1,18 @@
+import matplotlib.pyplot as plt
+
 print("geia sou andrea")
 
 print("TITANIC")
+
+def plotsomething():
+    a=[1,2,3,10]
+
+    b=[4,8,12,16]
+
+    plt.figure()
+    plt.plot(a,b)
+
+    plt.show()
+    return
+
+plotsomething()
