2823d5f839e27641c370dddb48d845cfb1615a91
kal-yi <johnkalargaris@gmail.com>
2021-02-01 20:41:54 +0000

    Long function with IFs replaced.
    Titanic result 0.792

--- Titanic_RF.py
+++ Titanic_RF.py
@@ -20,318 +20,25 @@ def load_data(X_filepath, X_test_filepath):
     return X, y, X_test, X_full
 
 
-def fam_relationship(all_data2, fam_size, SibSp, Parch, fam_number, index_num):
+def get_fam_relationship(all_data2, fam_size, Age, Title, fam_number):
 
-    # Avoid duplicating work - relationship has been defined when checking the family
-    if all_data2["fam_relationship"].loc[index_num] != "no_data":
-        return all_data2["fam_relationship"].loc[index_num]
+    if Age <= 17:
+        return "child"
 
-    elif all_data2["Age"].loc[index_num] <= 15:
-        all_data2["fam_relationship"].loc[index_num] = "child"
-        return all_data2["fam_relationship"].loc[index_num]
-
-    # 1 person
     elif fam_size == 1:
-        if (SibSp == 0) & (Parch == 0):
-            all_data2["fam_relationship"].loc[index_num] = "alone"
-            return all_data2["fam_relationship"].loc[index_num]
-
-        elif (SibSp == 0) & (Parch == 1):
-            if (all_data2["Title"].loc[index_num] == "Mrs") & (
-                all_data2["Age"].loc[index_num] <= 50
-            ):
-                all_data2["fam_relationship"].loc[index_num] = "mother"
-                return all_data2["fam_relationship"].loc[index_num]
-            elif all_data2["Age"].loc[index_num] <= 17:
-                all_data2["fam_relationship"].loc[index_num] = "child"
-                return all_data2["fam_relationship"].loc[index_num]
-            else:
-                all_data2["fam_relationship"].loc[index_num] = "husband or sibling"
-                return all_data2["fam_relationship"].loc[index_num]
-        else:
-            all_data2["fam_relationship"].loc[index_num] = "husband or sibling"
-            return all_data2["fam_relationship"].loc[index_num]
-
-    # 2 people
-    elif fam_size == 2:
-        list_fam_2 = []
-        list_fam_2 = all_data2[
-            all_data2["family_numbered"] == fam_number
-        ].index.tolist()  # get the indexes of all family members
-
-        if (all_data2["SibSp"].loc[list_fam_2[0]] == 1) & (
-            all_data2["SibSp"].loc[list_fam_2[1]] == 1
-        ):
-            all_data2["fam_relationship"].loc[list_fam_2[0]] = "husband or sibling"
-            all_data2["fam_relationship"].loc[list_fam_2[1]] = "husband or sibling"
-            return all_data2["fam_relationship"].loc[index_num]
-
-        elif (all_data2["Parch"].loc[list_fam_2[0]] == 1) & (
-            all_data2["Parch"].loc[list_fam_2[1]] == 1
-        ):
-            if (
-                all_data2["Age"].loc[list_fam_2[0]]
-                > all_data2["Age"].loc[list_fam_2[1]]
-            ):
-                if all_data2["Title"].loc[list_fam_2[0]] == "Mrs":
-                    all_data2["fam_relationship"].loc[list_fam_2[0]] = "mother"
-                    all_data2["fam_relationship"].loc[list_fam_2[1]] = "child"
-                    return all_data2["fam_relationship"].loc[
-                        index_num
-                    ]  # mother or child
-                else:
-                    all_data2["fam_relationship"].loc[list_fam_2[0]] = "father"
-                    all_data2["fam_relationship"].loc[list_fam_2[1]] = "child"
-                    return all_data2["fam_relationship"].loc[
-                        index_num
-                    ]  # father or child
-            else:
-                if all_data2["Title"].loc[list_fam_2[1]] == "Mrs":
-                    all_data2["fam_relationship"].loc[list_fam_2[1]] = "mother"
-                    all_data2["fam_relationship"].loc[list_fam_2[0]] = "child"
-                    return all_data2["fam_relationship"].loc[
-                        index_num
-                    ]  # mother or child
-                else:
-                    all_data2["fam_relationship"].loc[list_fam_2[1]] = "father"
-                    all_data2["fam_relationship"].loc[list_fam_2[0]] = "child"
-                    return all_data2["fam_relationship"].loc[
-                        index_num
-                    ]  # father or child
-
-        elif (SibSp == 0) & (Parch == 0):
-            all_data2["fam_relationship"].loc[index_num] = "alone"
-            return all_data2["fam_relationship"].loc[index_num]
-
-        else:
-            all_data2["fam_relationship"].loc[index_num] = "husband or sibling"
-            return all_data2["fam_relationship"].loc[index_num]
-
-    # 3 people
-    elif fam_size == 3:
-        list_fam_3 = []
-        list_fam_3 = all_data2[
-            all_data2["family_numbered"] == fam_number
-        ].index.tolist()  # get the indexes of all family members
-
-        if (all_data2["SibSp"].loc[index_num] == 0) & (
-            all_data2["Parch"].loc[index_num] == 2
-        ):
-            list_fam_3.remove(index_num)
-            if (
-                (all_data2["Age"].loc[index_num] + float(12))
-                <= all_data2["Age"].loc[list_fam_3[0]]
-            ) & (
-                (all_data2["Age"].loc[index_num] + float(12))
-                <= all_data2["Age"].loc[list_fam_3[1]]
-            ):
-                all_data2["fam_relationship"].loc[index_num] = "child"
-                if all_data2["Title"].loc[list_fam_3[0]] == "Mrs":
-                    all_data2["fam_relationship"].loc[list_fam_3[0]] = "mother"
-                    all_data2["fam_relationship"].loc[list_fam_3[1]] = "father"
-                    return all_data2["fam_relationship"].loc[index_num]  # child
-                else:
-                    all_data2["fam_relationship"].loc[list_fam_3[0]] = "father"
-                    all_data2["fam_relationship"].loc[list_fam_3[1]] = "mother"
-                    return all_data2["fam_relationship"].loc[index_num]  # child
-
-            elif all_data2["Age"].loc[index_num] <= 17:
-                all_data2["fam_relationship"].loc[index_num] = "child"
-                return all_data2["fam_relationship"].loc[
-                    index_num
-                ]  # not 100% accurate (it could be spend more time on it)
-            else:
-                if all_data2["Title"].loc[index_num] == "Mrs":
-                    all_data2["fam_relationship"].loc[index_num] = "mother"
-                    return all_data2["fam_relationship"].loc[index_num]
-                else:
-                    all_data2["fam_relationship"].loc[index_num] = "father"
-                    return all_data2["fam_relationship"].loc[index_num]
-
-        elif (all_data2["SibSp"].loc[index_num] == 1) & (
-            all_data2["Parch"].loc[index_num] == 1
-        ):
-            if all_data2["Age"].loc[index_num] <= 17:
-                all_data2["fam_relationship"].loc[index_num] = "child"
-                return all_data2["fam_relationship"].loc[
-                    index_num
-                ]  # not 100% accurate (it could be spend more time on it)
-
-            elif all_data2["Title"].loc[index_num] == "Mrs":
-                all_data2["fam_relationship"].loc[index_num] = "mother"
-                return all_data2["fam_relationship"].loc[
-                    index_num
-                ]  # not 100% accurate (it could be spend more time on it)
-
-            else:
-                all_data2["fam_relationship"].loc[index_num] = "husband or sibling"
-                return all_data2["fam_relationship"].loc[index_num]  # sibling
-
-        else:
-            all_data2["fam_relationship"].loc[index_num] = "husband or sibling"
-            return all_data2["fam_relationship"].loc[index_num]
-
-    # 4 people
-    elif fam_size == 4:
-        list_fam_4 = []
-        list_fam_4 = all_data2[
-            all_data2["family_numbered"] == fam_number
-        ].index.tolist()  # get the indexes of all family members
-
-        if (all_data2["SibSp"].loc[index_num] == 3) & (
-            all_data2["Parch"].loc[index_num] == 0
-        ):
-            all_data2["fam_relationship"].loc[index_num] = "husband or sibling"
-            return all_data2["fam_relationship"].loc[index_num]  # sibling
-
-        elif (all_data2["SibSp"].loc[index_num] == 2) & (
-            all_data2["Parch"].loc[index_num] == 1
-        ):
-            all_data2["fam_relationship"].loc[index_num] = "child"
-            return all_data2["fam_relationship"].loc[index_num]
-
-        elif (all_data2["SibSp"].loc[index_num] == 0) & (
-            all_data2["Parch"].loc[index_num] == 3
-        ):
-            if (all_data2["Title"].loc[index_num] == "Mrs") or (
-                all_data2["Title"].loc[index_num] == "Ms"
-            ):
-                all_data2["fam_relationship"].loc[index_num] = "mother"
-                return all_data2["fam_relationship"].loc[index_num]
-            else:
-                all_data2["fam_relationship"].loc[index_num] = "father"
-                return all_data2["fam_relationship"].loc[index_num]
-
-        elif (all_data2["Title"].loc[index_num] == "Mrs") & (
-            all_data2["Age"].loc[index_num] <= 50
-        ):
-            all_data2["fam_relationship"].loc[index_num] = "mother"
-            return all_data2["fam_relationship"].loc[index_num]
-
-        else:
-            all_data2["fam_relationship"].loc[index_num] = "husband or sibling"
-            return all_data2["fam_relationship"].loc[index_num]  # sibling
-
-    # 5 people
-    elif fam_size == 5:
-
-        if (all_data2["SibSp"].loc[index_num] == 3) & (
-            all_data2["Parch"].loc[index_num] == 1
-        ):
-            all_data2["fam_relationship"].loc[index_num] = "child"
-            return all_data2["fam_relationship"].loc[index_num]
-
-        elif (all_data2["SibSp"].loc[index_num] == 2) & (
-            all_data2["Parch"].loc[index_num] == 2
-        ):
-            all_data2["fam_relationship"].loc[index_num] = "child"
-            return all_data2["fam_relationship"].loc[index_num]
-
-        elif (
-            (all_data2["SibSp"].loc[index_num] == 1)
-            & (all_data2["Parch"].loc[index_num] == 3)
-        ) or (
-            (all_data2["SibSp"].loc[index_num] == 0)
-            & (all_data2["Parch"].loc[index_num] == 4)
-        ):
-            if (
-                (all_data2["Title"].loc[index_num] == "Mrs")
-                or (all_data2["Title"].loc[index_num] == "Ms")
-            ) & (all_data2["Age"].loc[index_num] <= 50):
-                all_data2["fam_relationship"].loc[index_num] = "mother"
-                return all_data2["fam_relationship"].loc[index_num]
-            else:
-                all_data2["fam_relationship"].loc[index_num] = "husband or sibling"
-                return all_data2["fam_relationship"].loc[index_num]  # sibling
-
-        else:
-            all_data2["fam_relationship"].loc[index_num] = "husband or sibling"
-            return all_data2["fam_relationship"].loc[index_num]  # sibling
-
-    # 6 people
-    elif fam_size == 6:
-
-        if (
-            (all_data2["SibSp"].loc[index_num] == 1)
-            & (all_data2["Parch"].loc[index_num] == 4)
-        ) or (
-            (all_data2["SibSp"].loc[index_num] == 0)
-            & (all_data2["Parch"].loc[index_num] == 5)
-        ):
-            if (
-                (all_data2["Title"].loc[index_num] == "Mrs")
-                or (all_data2["Title"].loc[index_num] == "Ms")
-            ) & (all_data2["Age"].loc[index_num] <= 50):
-                all_data2["fam_relationship"].loc[index_num] = "mother"
-                return all_data2["fam_relationship"].loc[index_num]
-            else:
-                all_data2["fam_relationship"].loc[index_num] = "father"
-                return all_data2["fam_relationship"].loc[index_num]
-
-        else:
-            all_data2["fam_relationship"].loc[index_num] = "husband or sibling"
-            return all_data2["fam_relationship"].loc[index_num]  # sibling
-
-    # 7 people
-    elif fam_size == 7:
-
-        if (
-            (all_data2["SibSp"].loc[index_num] == 1)
-            & (all_data2["Parch"].loc[index_num] == 5)
-        ) or (
-            (all_data2["SibSp"].loc[index_num] == 0)
-            & (all_data2["Parch"].loc[index_num] == 6)
-        ):
-            if (
-                (all_data2["Title"].loc[index_num] == "Mrs")
-                or (all_data2["Title"].loc[index_num] == "Ms")
-            ) & (all_data2["Age"].loc[index_num] <= 50):
-                all_data2["fam_relationship"].loc[index_num] = "mother"
-                return all_data2["fam_relationship"].loc[index_num]
-            else:
-                all_data2["fam_relationship"].loc[index_num] = "father"
-                return all_data2["fam_relationship"].loc[index_num]
-
-        else:
-            all_data2["fam_relationship"].loc[index_num] = "husband or sibling"
-            return all_data2["fam_relationship"].loc[index_num]
-
-    # 8 people
-    elif fam_size == 8:
-
-        if (
-            (all_data2["SibSp"].loc[index_num] == 1)
-            & (all_data2["Parch"].loc[index_num] == 6)
-        ) or (
-            (all_data2["SibSp"].loc[index_num] == 0)
-            & (all_data2["Parch"].loc[index_num] == 7)
-        ):
-            if (
-                (all_data2["Title"].loc[index_num] == "Mrs")
-                or (all_data2["Title"].loc[index_num] == "Ms")
-            ) & (all_data2["Age"].loc[index_num] <= 50):
-                all_data2["fam_relationship"].loc[index_num] = "mother"
-                return all_data2["fam_relationship"].loc[index_num]
-            else:
-                all_data2["fam_relationship"].loc[index_num] = "father"
-                return all_data2["fam_relationship"].loc[index_num]
-
-        else:
-            all_data2["fam_relationship"].loc[index_num] = "husband or sibling"
-            return all_data2["fam_relationship"].loc[index_num]  # sibling
-
-    # >=9 people
-    elif fam_size >= 9:
-        all_data2["fam_relationship"].loc[index_num] = "husband or sibling"
-        return all_data2["fam_relationship"].loc[index_num]
-
-    elif (SibSp == 0) & (Parch == 0):
-        all_data2["fam_relationship"].loc[index_num] = "alone"
-        return 0, "alone"
+        return "alone"
 
     else:
-        all_data2["fam_relationship"].loc[index_num] = "I dont know"
-        return all_data2["fam_relationship"].loc[index_num]
+        list_fam_Ages = []
+        list_fam_Ages = all_data2[
+            all_data2["family_numbered"] == fam_number
+        ].Age.tolist()
+        family_analysis = ["child" if x <= 17 else "adult" for x in list_fam_Ages]
+
+        if (Title == "Mrs") or (Title == "Ms") & ("child" in family_analysis):
+            return "mother"
+        else:
+            return "husband or sibling or father"
 
 
 def data_preprocessing(X, X_test):
@@ -411,40 +118,23 @@ def data_preprocessing(X, X_test):
     )
 
     all_data["family_numbered"] = all_data.groupby(["Surname", "Ticket"]).ngroup()
-    all_data["Family_size"] = all_data.family_numbered.map(
-        all_data.groupby("family_numbered").size()
-    )  # family size calculated
-    all_data["Family_size2"] = (
-        all_data["SibSp"] + all_data["Parch"] + 1
-    )  # true family size
+    all_data["Family_size2"] = all_data["SibSp"] + all_data["Parch"] + 1
 
     # ----------------------------------------- Column "fam_relationship" -----------------------------------------
-    # Column created to identify the children, mothers, fathers, etc.
+    # Column created to identify the children, mothers, etc.
 
     all_data2 = all_data.copy()
     all_data2["fam_relationship"] = "no_data"
     all_data2["fam_relationship"] = all_data2.apply(
-        lambda column: fam_relationship(
+        lambda column: get_fam_relationship(
             all_data2,
-            column["Family_size"],
-            column["SibSp"],
-            column["Parch"],
+            column["Family_size2"],
+            column["Age"],
+            column["Title"],
             column["family_numbered"],
-            column.name,
         ),
         axis=1,
     )
-
-    # Treat exemptions correctly
-    mother_list = [86, 137, 248, 335, 541, 581, 601, 775, 996, 1045, 1133, 1222]
-    for mother in mother_list:
-        all_data2["fam_relationship"].loc[mother] = "mother"
-
-    child_list = [418, 944]
-    for child in child_list:
-        all_data2["fam_relationship"].loc[child] = "child"
-
-    all_data2["fam_relationship"].loc[146] = "husband or sibling"
 
     # Finalize X and X_test
     all_data2 = all_data2.drop(
@@ -454,7 +144,6 @@ def data_preprocessing(X, X_test):
             "Surname",
             "Ticket",
             "family_numbered",
-            "Family_size2",
             "SibSp",
             "Parch",
         ],
@@ -474,7 +163,7 @@ def data_preprocessing(X, X_test):
         (["Cabin"], None),
         (["Embarked"], OneHotEncoder()),
         (["Title"], OneHotEncoder()),
-        (["Family_size"], None),
+        (["Family_size2"], None),
         (["fam_relationship"], OneHotEncoder()),
     ]
 
@@ -508,11 +197,11 @@ def apply_k_means(X, X_test):
 def hyperparams_selection(X, y):
 
     grid_params = {
-        "n_estimators": range(355, 360, 5),
+        "n_estimators": range(305, 310, 5),
         "criterion": ["gini"],  # ,"entropy"]
         "max_depth": range(10, 11, 1),
-        "max_features": range(19, 21, 1),
-        "max_leaf_nodes": range(16, 17, 1),
+        "max_features": range(14, 15, 1),
+        "max_leaf_nodes": range(11, 12, 1),
     }
 
     model_grid = GridSearchCV(
@@ -562,7 +251,7 @@ def Titanic_RF():
 
     rf_model = model(X, y, rf_hyperparams)
 
-    pred_file = save_Y_test_pred(rf_model, X_test)
+    save_Y_test_pred(rf_model, X_test)
 
 
 if __name__ == "__main__":
